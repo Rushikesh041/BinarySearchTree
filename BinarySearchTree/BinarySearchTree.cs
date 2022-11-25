785c4dd7176d409adf1d549e6d51bc80cb22a7ab
Rushikesh041 <jadhavrushi041@gmail.com>
2022-11-26 05:21:58 +0530

    UC1Create

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTree<T> where T : IComparable<T>
+    {
+        public Node<T> Root;
+        public void Add(T data)
+        {
+            Node<T> parent = null, current = Root;
+
+            while (current != null)
+            {
+                parent = current;
+                if (current.Data.CompareTo(data) >= 0)
+                    current = current.leftNode;
+                else if (data.CompareTo(current.Data) >= 0)
+                    current = current.rightNode;
+                else
+                {
+                    Console.WriteLine($"{data} is already present in Binary Tree");
+                }
+            }
+
+            Node<T> newNode = new Node<T>(data);
+
+            if (this.Root == null)
+                this.Root = newNode;
+            else
+            {
+                if (parent.Data.CompareTo(data) >= 0)
+                    parent.leftNode = newNode;
+                else
+                    parent.rightNode = newNode;
+            }
+        }
+        public void DisplayInorder(Node<T> parent)
+        {
+            if (parent != null)
+            {
+                DisplayInorder(parent.leftNode);
+                Console.Write(parent.Data + " ");
+                DisplayInorder(parent.rightNode);
+            }
+        }
+    }
+}
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace BinarySearchTree
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree<int> tree = new BinarySearchTree<int>();
+            tree.Add(56);
+            tree.Add(30);
+            tree.Add(70);
+
+            Console.WriteLine("******** Elements in Binary Trees ********");
+            tree.DisplayInorder(tree.Root);
+
+        }
+    }
+}
